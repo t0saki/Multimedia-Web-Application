d367f548473d8b17ac65cde74eed1e5b6c93bd75
duolaameng <908992763@qq.com>
2023-04-05 05:26:29 +0800

    yg_test

---
+++ function.js
@@ -0,0 +1,26 @@
+const leftButton = document.querySelector(".left-btn");
+const rightButton = document.querySelector(".right-btn");
+const heroImage = document.querySelector(".hero-image");
+
+const imagePaths = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg'
+  ];
+let imageIndex = 1;
+
+leftButton.addEventListener("click", () => {
+  imageIndex--;
+  if (imageIndex < 1) {
+    imageIndex = 3;
+  }
+  heroImage.src = `image${imageIndex}.jpg`;
+});
+
+rightButton.addEventListener("click", () => {
+  imageIndex++;
+  if (imageIndex > 3) {
+    imageIndex = 1;
+  }
+  heroImage.src = `image${imageIndex}.jpg`;
+});
